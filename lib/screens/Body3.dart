89edbca6d3eb08c230b3b9717633f7a52252e004
Amany Naser <114959361+AmanyNasser@users.noreply.github.com>
2023-08-10 22:16:24 +0200

    Added prework files to repository

---
+++ lib/screens/Body3.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Body3 extends StatefulWidget {
+  const Body3({super.key});
+
+  @override
+  State<Body3> createState() => _Body3State();
+}
+
+class _Body3State extends State<Body3> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("page3"),
+    );
+  }
+}
